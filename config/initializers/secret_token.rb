b1139eeba363025859e27aac31a1d5a99f91c721
Glauber Prado <smade4@gmail.com>
2010-07-29 23:10:22 -0300

    commit inicial, rails sem prototype e test_unit c/ sqlite3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BuddyList::Application.config.secret_token = 'e74018e26c32bc8d29387f3bd1941f04a019763d3d0e8e65c8f6b94fb4525ede8b3784c3860c7c378fbf691fc0f075d612d51ebdbdcc971071df87991b165edb'
